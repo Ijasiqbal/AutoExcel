b4871a3c7705e9e150a931b21a4e5fe442215d20
Ijas Iqbal <ijasiqbal4@gmail.com>
2023-12-05 00:45:36 +0530

    alignment issues fixed

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -44,12 +44,10 @@ export default function Chat() {
     const sentData = `You are a text parser which takes unstructred data and return structured data in the form of json object,Given a context ,column names and unstructured data for a  csv file,  return json object for the data as ,\n\n{\n column1: relevant data,\ncolumn2: relevant data,\n......\n}\nif data is not present use null, return only the json object.\n\n Example for  a car dealership the columns are brand , make, manufacturing year,  ownership number, price, km run.\n\nunstructured data: '2017 Sigma 4 auto 4by4 delhi 1st 1.29lac km done, 21.5 lac '.\n\nstructured output: {\nmanufacturing year: 2017,\nkm_run: 129000,\nprice: 2150000,\nownership_no:1st,\nmake: null,\nbrand:null\n}\n\n\ngiven context:${industry}, given columns: ${columnNames.join(' , ')}, \n given unstructured data: '${prompt}'.\ngive json output.` // Update with your prompt
 
     try {
-        console.log(sentData)
       const result = await openai.chat.completions.create({
         model: 'gpt-3.5-turbo',
         messages: [{ role: 'system', content: sentData }],
       });
-      console.log('API response:', result.choices[0].message.content);
       const jsonResponse = result.choices[0]?.message?.content ? JSON.parse(result.choices[0].message.content) : null;
       console.log('API response:', jsonResponse);
       setApiResponse(jsonResponse);
